9bef777c31b66f822a79fff54b81f735f258f162
dashu <xubigshu@163.com>
2016-04-10 16:31:26 +0800

    修改demo版本

---
+++ demo/helloworld/sync_db.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# @author   xubigshu@gmail.com
+# @site     http://www.51lu.me
+# @date     2016-1-9
+#
+
+# import all model for syncdb
+from models.main_models import *
+
+from tornadopy.db.dbalchemy import Connector
+
+
+def syncdb():
+    for k, conn in Connector.conn_pool.items():
+        conn.create_db()
